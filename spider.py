24d7083fdfbd4a22966727080e683dfcbe133ef4
Miloxing <1650658858@qq.com>
2018-02-23 19:02:13 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -6,8 +6,9 @@ import json
 
 
 def get_one_page(url):
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
     try:
-        response = requests.get(url)
+        response = requests.get(url,headers=headers)
         if response.status_code == 200:
             return response.text
         return None
